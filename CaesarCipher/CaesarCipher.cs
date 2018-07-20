b49277db5b8826f912eac8f70be1471bd1d14c59
Anandkumar <anandkumar316sw@gmail.com>
2018-07-20 11:25:32 +0530

    Assign-5 Solution

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace CaesarCipher
 {
@@ -7,6 +9,21 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            StringBuilder caesar = new StringBuilder();
+            int x;
+            foreach(char c in text)
+            {
+                if(Char.IsLetter(c))
+                {
+                    x = Char.IsUpper(c) ? ((c + shiftKey - 'A') % 26) +'A' : ((c + shiftKey - 'a') % 26)+'a';
+                    caesar.Append((char)x);
+                }
+                else
+                {
+                    caesar.Append(c);
+                }
+            }
+            return caesar.ToString();
             throw new NotImplementedException("You need to implement this function.");
         }
     }
